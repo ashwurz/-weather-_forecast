5cef5110246eeb5e9158b0a41dc77de924f9d169
Ashwurz <helmuthfilho001@gmail.com>
2019-07-07 19:42:49 -0300

    Alteração da url da api

--- lib/ApiConnections/HgWeatherConnection.dart
+++ lib/ApiConnections/HgWeatherConnection.dart
@@ -2,7 +2,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class HgWeatherConnection{
-  String _url = "https://api.hgbrasil.com/weather?key=d5a4b5ec&user_ip=remote";
+  String _url = "https://api.hgbrasil.com/weather?key=fe71e003&user_ip=remote";
+
 
   String searchString;
 
@@ -19,6 +20,6 @@ class HgWeatherConnection{
   }
 
   String _getSearchResponse(String search){
-    return "https://api.hgbrasil.com/weather?key=d5a4b5ec&city_name=$search";
+    return "https://api.hgbrasil.com/weather?key=fe71e003&city_name=$search";
   }
 }
